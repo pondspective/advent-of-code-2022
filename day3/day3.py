5605b2b47eae7bca51d3a0b0508720f8b0267a8d
pondspective <puvitza.135@gmail.com>
2022-12-11 22:06:47 +0700

    :sparkles: Part 2 done

--- day3/day3.py
+++ day3/day3.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import math
 
 f = open('input.txt','r')
 input_text = f.read()
@@ -10,9 +11,13 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"""
 
-def process_rucksack(input):
-    return [spilt_compartments(line) for line in input.split('\n')]
+def process_rucksack(input, spilt_compartment = True):
+    if spilt_compartment:
+        return [spilt_compartments(line) for line in input.split('\n')]
+    else:
+        return [line for line in input.split('\n')]
 
+# split rucksack into 2 compartments
 def spilt_compartments(str):
     return [str[:int(len(str)/2)],str[int(len(str)/2):]]
 
@@ -22,7 +27,16 @@ def to_priority(rucksack):
         priority = ord(item) - 38 if item.isupper() else ord(item) - 96
         res.append(priority)
     return res
-            
+
+# group three rucksack into badges
+def group_badges(rucksacks):
+    badges = []
+    for n in range (0,math.ceil(len(rucksacks)/3)):
+        tmp = [rucksacks[3*n + i] for i in range(0,3)]
+        print(tmp)
+        badges.append(tmp)
+    return badges
+
 
 def find_duplicate_number(c1, c2):
     c1 = sorted(c1)
@@ -40,8 +54,41 @@ def find_duplicate_number(c1, c2):
                 print(f"c2 -> {j},{c2[j]}")
                 j += 1
 
+def find_many_duplicate_numbers(c1, c2):
+    c1 = sorted(c1)
+    c2 = sorted(c2)
+
+    print(c1, c2)
+
+    dup_nums = []
+    for i in range(0,int(len(c1))):
+        for j in range(0,int(len(c2))):
+            if c1[i] == c2[j]:
+                print(c1[i] ,'is duplicate')
+                dup_nums.append(c1[i])
+                j += 1
+            elif c1[i] > c2[j]:
+                print(f"c1 -> {i},{c1[i]}")
+                print(f"c2 -> {j},{c2[j]}")
+                j += 1
+    
+    print(f"Duplicate Nums List -> {dup_nums}")
+    return(dup_nums)
+
 def part1(input):
     priority_scores = [find_duplicate_number(to_priority(compartment[0]), to_priority(compartment[1])) for compartment in process_rucksack(input)]
     return reduce(lambda x, y: x+y,priority_scores)
 
+def part2(input):
+    res = []
+    badges = group_badges(process_rucksack(input, spilt_compartment=False))
+    for badge in badges:
+        badge = [to_priority(c) for c in badge]
+        dup_1_2 = find_many_duplicate_numbers(badge[0], badge[1])
+        res.append(find_duplicate_number(dup_1_2, badge[2]))
+    
+    return reduce(lambda x, y: x+y,res)
+
 print(part1(input_text))
+print("*************************************")
+print(part2(input_text))
