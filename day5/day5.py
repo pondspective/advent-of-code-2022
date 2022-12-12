697cacfc2f272acfa28d0d1b7e293a9cf519da6a
pondspective <puvitza.135@gmail.com>
2022-12-12 16:59:39 +0700

    :sparkles: Day5 Part1 Done

--- day5/day5.py
+++ day5/day5.py
@@ -1,43 +1,71 @@
 from collections import defaultdict
 import re
 import itertools
+from parse import compile
+
+# Expected Result
+#     [D]
+# [N] [C]    
+# [Z] [M] [P]
+#  1   2   3 
+
+# {
+#     1 : 'NZ',
+#     2 : 'DCM',
+#     3 : 'P'
+# }
 
 def parse_input(filename):
     with open(filename, "r", encoding="utf8") as f:
         stacks_str, commands = f.read().split("\n\n")
 
     # transform command
-    regex = r"move (\d) from (\d) to (\d)"
-    matches = re.finditer(regex, commands, re.MULTILINE)
-    command_list = []
-    for i in matches:
-        command_list.append([int(param) for param in i.groups()])
+    extract_command = compile("move {:d} from {:d} to {:d}")
+    command_list = [list(extract_command.parse(command)) for command in commands.split('\n')]
 
     # transform stacks
     s_items, s_number = stacks_str.split("\n ")
-    # [|] is empty stack
-    stack_items = list(map(lambda x: x.strip().split(" "), s_items.replace("    ", " [|]").split("\n")))
-    stack_items_tsp =  [stack[::-1] for stack in list(map(list, itertools.zip_longest(*stack_items, fillvalue=None)))]
+    rows = s_items.split("\n")
+    print(rows)
     stack_nb = [int(num) for num in s_number.strip().split("   ")]
-    stacks_dict = dict(zip(stack_nb, stack_items_tsp))
+    stacks_dict = {}
+    for stack in stack_nb:
+        items = []
+        for i in range(len(rows)-1, -1, -1):
+            pos = 4*stack-3
+            if (len(rows[i]) > pos):
+                if (rows[i][pos] != ' '):
+                    print(f"Row -> {rows[i]} , Item -> {rows[i][pos]}")
+                    items.append(rows[i][pos])
+        stacks_dict.update({stack:items})
 
     return stacks_dict, command_list
 
-stacks, commands = parse_input('test_input.txt')
-print(stacks)
-for command in commands:
-    round, source, des = command
-    print(f"Round -> {round} / Source -> {source} / Dest -> {des}")
+def move_item(stack ,source, des):
+    if len(stack[source]) != 0:
+        item = stack[source].pop()
+        stack[des].append(item)
+        print(f"Move {item} from {source} to {des}")
+    return stack
+
+def move_item_n_times(stack ,source, des, round):
+    print(f"+++ Move from {source} to {des} x{round} +++")
     for i in range(0,round):
-        if (len(stacks[source]) == 0):
-            break
-        if(stacks[source][-1] == '[|]'):
-            stacks[source].pop()
-        
-        item = stacks[source].pop()
-        stacks[des].append(item)
-        print(f"Move {item} from {source} to {des} , #{i+1}")
+        stack = move_item(stack, source, des)
+    return stack
+
+def part1(path):
+    stacks, commands = parse_input(path)
     print(stacks)
+    for command in commands:
+        round, source, des = command
+        stacks = move_item_n_times(stacks, source, des, round)
+    print("FINAL STACK")
+    print(stacks)
+    final_word = "".join([stack[-1] for stack in stacks.values()])
+    return final_word
+
+print(part1('input.txt'))
 
         
 
